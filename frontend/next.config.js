75c9cc68cfb7623427d334e46c4531ab254b2975
gomgu <gomgu@example.com>
2026-05-16 17:11:53 +0530

    build: ignore eslint and typescript errors during nextjs build

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -11,6 +11,12 @@ const nextConfig = {
     ],
   },
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production',
   },
